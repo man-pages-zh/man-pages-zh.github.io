1cc179597c8f11fbb778518ddccd0bbc2d2dad94
Darcy Shen <sadhen@zoho.com>
2015-06-02 19:29:00 +0800

    update javascript

--- js/collate.js
+++ js/collate.js
@@ -1,46 +1,45 @@
 $(document).ready(function() {
-    var docName = document.location.pathname.match(/[^\/]+$/)[0];
+    var docName = document.location.pathname.slice(0);
 
-    $('body').prepend('<div id="raw" hidden></div>');
+    $('body').append('<div id="raw" hidden></div>');
     var raw = $("#raw");
 
-    raw.load("../man1-raw/" + docName, function(response, status, xhr) {
+    if (docName.slice(1, 4) == "raw")
+        var shuffle = "man" + docName.slice(4)
+    else
+        var shuffle = "raw" + docName.slice(4)
+
+    raw.load("../" + shuffle + " #main", function(response, status, xhr) {
         if (status === "error") {
             console.log("error loading English man page.");
         }
         else {
-            console.log(docName);
             addTooltip();
         }
     });
     function addTooltip() {
-        var pre = $("#raw pre");
-
-        var name = pre.slice(1, 2).html();
-
-        var description = [];
+        var name = $("#raw div.content:first").text()
         // description of English man page
-        pre.slice(3, 4).contents().filter(function() {
-            if (this.nodeType === 3) {
-                var itemTxt = $(this)
-                    .text()
-                    .replace(/^[\s]+/, '')
-                    .replace(/[\n\t]+/gm, '');
-                if (itemTxt.length > 2) {
-                    description.push(itemTxt);
-                }
-            }
-        });
+        var description = $("#raw dd")
+        // paragraph
+        var para = $("#raw p")
 
         // add tooltip to NAME
-        $(".content:first")
+        $("#main div.content:first")
             .attr('title', 'More')
             .wrap('<a href="#" class="tooltip" title="' + name + '"></a>');
 
         // add tooltip to other DESCRIPTION
         $("dl:first dd").each(function (i) {
             $(this).attr('title', 'More');
-            $(this).wrap('<a href="#" class="tooltip" title="' + description[i+1] + '"></a>');
+            $(this).wrap('<a href="#" class="tooltip" title="' + description.slice(i, i+1).text() + '"></a>');
+        });
+        
+        $("#main p").each(function (i) {
+            if (i < para.size()) {
+                $(this).attr('title', 'More');
+                $(this).wrap('<a href="#" class="tooltip" title="' + para.slice(i, i+1).text() + '"></a>');
+            }
         });
     }
 
